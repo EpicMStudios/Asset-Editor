bf0c6994a84ffcd9426bc343b56065b3ea9680cf
epicmoostudios <epicmoostudios@gmail.com>
2015-01-11 14:18:41 -0500

    First Commit. Completely fresh project.

---
+++ core/src/com/epicmstudios/asseteditor/AssetEditor.java
@@ -0,0 +1,22 @@
+package com.epicmstudios.asseteditor;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class AssetEditor extends ApplicationAdapter {
+
+	
+	
+	@Override
+	public void create () {
+
+	}
+
+	@Override
+	public void render () {
+
+	}
+}
